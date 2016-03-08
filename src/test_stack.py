8d42740b7c2ee9eddbf5e16f9d45637b98059ec4
Kent Ross <root.main@gmail.com>
2016-03-08 14:09:04 -0800

    minor style changes

--- src/test_stack.py
+++ src/test_stack.py
@@ -2,26 +2,25 @@
 import pytest
 
 
+# We are testing invalid arguments
+# noinspection PyArgumentList
 def test_init():
     from stack import Stack
     Stack()
     Stack([1])
-    Stack([1,2,3,4])
+    Stack([1, 2, 3, 4])
     Stack("abc")
     with pytest.raises(TypeError):
         Stack(1)
     with pytest.raises(TypeError):
-        Stack(1,2)
+        Stack(1, 2)
     with pytest.raises(TypeError):
-        Stack([1,2],3,4)
+        Stack([1, 2], 3, 4)
 
 
 def test_push():
     pass
 
+
 def test_pop():
     pass
-
-
-
-
