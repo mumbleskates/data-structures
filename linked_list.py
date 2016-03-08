f3a6992a54abd26b47bece53ee412bf9646e28ff
Kent Ross <root.main@gmail.com>
2016-03-07 16:19:46 -0800

    added search, remove, and display, with tests for search & remove

--- linked_list.py
+++ linked_list.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+
 class Node(object):
     def __init__(self, data=None, point=None):
         self.data = data
@@ -23,27 +24,49 @@ class LinkedList(object):
             self.length += 1
 
         def pop(self):
-            firstNode = self.head.point
-            self.head.point = firstNode.point
+            first_node = self.head.point
+            self.head.point = first_node.point
             self.length -= 1
-            return firstNode.data
+            return first_node.data
 
         def size(self):
             return self.length
 
         def search(self, value):
-            pass
+            """
+            Return the first node in the list with the data `value`,
+            or None if not found
+            """
+            node = self.head.point
+            while node is not self.head:
+                if node.data == value:
+                    return node
+                node = node.point
+            return None
 
         def remove(self, node):
-            pass
+            """
+            Remove the given node from the list. Return True if it was removed,
+            False if it was not found.
+            """
+            prev = self.head
+            current = prev.point
+            while current is not self.head:
+                if current is node:
+                    prev.point = current.point
+                    self.length -= 1
+                    return True
+                # this isn't the node, advance
+                prev, current = current, current.point
+            return False
 
         def display(self):
-            pass
+            print(tuple(self))
 
 
 # DONE: insert(val) will insert the value ‘val’ at the head of the list
 # DONE: pop() will pop the first value off the head of the list and return it.
 # DONE: size() will return the length of the list
-# TODO: search(val) will return the node containing ‘val’ in the list, if present, else None
-# TODO: remove(node) will remove the given node from the list, wherever it might be (node must be an item in the list)
-# TODO: display() will print the list represented as a Python tuple literal: “(12, ‘sam’, 37, ‘tango’)”
+# DONE: search(val) will return the node containing ‘val’ in the list, if present, else None
+# DONE: remove(node) will remove the given node from the list, wherever it might be (node must be an item in the list)
+# DONE: display() will print the list represented as a Python tuple literal: “(12, ‘sam’, 37, ‘tango’)”
--- test_linked_list.py
+++ test_linked_list.py
@@ -1,11 +1,12 @@
 # coding=utf-8
-
 import pytest
+
 
 def test_empty_list():
     from linked_list import LinkedList
-    LinkedList()
-    assert LinkedList().length == 0
+    a_list = LinkedList()
+    assert a_list.length == 0
+    assert list(a_list) == []
 
 
 def test_insert():
@@ -13,8 +14,10 @@ def test_insert():
     a_list = LinkedList()
     a_list.insert(1)
     assert list(a_list) == [1]
+    assert a_list.size() == 1
     a_list.insert(2)
     assert list(a_list) == [2, 1]
+    assert a_list.size() == 2
 
 
 TEST_POP = {
@@ -23,9 +26,49 @@ TEST_POP = {
     (123490872988582498572379827512423, 123490872988582498572379827512423)
 }
 
+
 @pytest.mark.parametrize("node_value, result", TEST_POP)
 def test_pop(node_value, result):
     from linked_list import LinkedList
     a_list = LinkedList()
     a_list.insert(node_value)
     assert a_list.pop() == result
+
+
+def test_search():
+    from linked_list import LinkedList
+    a_list = LinkedList()
+    assert a_list.search(0) is None
+    # insert some values
+    for i in range(10):
+        a_list.insert(i)
+    # prove those values are searchable
+    for i in range(10):
+        assert a_list.search(i).data == i
+    # prove the list is in the right order etc.
+    assert list(a_list) == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    # prove that values that have not been added cause None to return
+    assert a_list.search(100) is None
+
+
+def test_remove():
+    from linked_list import LinkedList
+    a_list = LinkedList()
+    assert a_list.remove(0) is False
+    # insert some values
+    for i in range(10):
+        a_list.insert(i)
+    assert list(a_list) == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    # get 7 and remove it
+    seven = a_list.search(7)
+    assert a_list.remove(seven) is True
+    # 7 has already been removed so this should return False
+    # because nothing happened
+    assert a_list.remove(seven) is False
+    assert list(a_list) == [9, 8, 6, 5, 4, 3, 2, 1, 0]
+    # remove all the even-numbered values
+    for node in a_list:
+        if node & 1 == 0:
+            assert a_list.remove(a_list.search(node)) is True
+    assert list(a_list) == [9, 5, 3, 1]
+    assert a_list.remove(100) is False
