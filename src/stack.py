2069562f32dc1ebbfa88afc26190cf2b159aafe8
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-08 13:57:06 -0800

    Added Stack __init__ test Added new file test_stack.py Added new file stack.py

---
+++ src/stack.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from linked_list import LinkedList
+
+
+class Stack(LinkedList):
+    """ Using LinkedList __init__ for Stack init method"""
+
+    def push(self, val):
+        pass
---
+++ src/test_stack.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+def test_init():
+    from stack import Stack
+    Stack()
+    Stack([1])
+    Stack([1,2,3,4])
+    Stack("abc")
+    with pytest.raises(TypeError):
+        Stack(1)
+    with pytest.raises(TypeError):
+        Stack(1,2)
+    with pytest.raises(TypeError):
+        Stack([1,2],3,4)
+
+
+def test_push():
+    pass
+
+def test_pop():
+    pass
+
+
+
+
