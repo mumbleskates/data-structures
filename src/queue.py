78e64d1a45fea4a4c5e23e58cd7cf3e0c0f8fd18
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-10 14:35:35 -0800

    Added doc strings

--- src/queue.py
+++ src/queue.py
@@ -8,18 +8,32 @@ class Queue(object):
         self._list = Dll(items)
         self._len = 0
 
-    def __len__(self, other):
+    def __len__(self):
+        """
+        return the size of the queue. Should return 0 if the queue is empty.
+        """
         return self._len
 
     def enqueue(self, item):
+        """
+        adds value to the queue
+        """
         self._len += 1
         self._list.append(item)
 
     def dequeue(self):
+        """
+        removes the correct item from the queue and returns its value (should raise an
+        error if the queue is empty)
+        """
         self._len -= 1
         return self._list.pop()
 
     def peek(self):
+        """
+        returns the next value in the queue without dequeueing it. If the queue is
+        empty, returns None
+        """
         if not self._len:
             return None
         else:
