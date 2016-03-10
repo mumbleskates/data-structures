fbe484e4b99e47f250174535f507318407564457
Kent Ross <root.main@gmail.com>
2016-03-10 14:43:07 -0800

    added tests for queue

--- src/test_queue.py
+++ src/test_queue.py
@@ -1,19 +1,74 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from builtins import range
 
 import pytest
 
 
-def test_enqueue(value):
-    assert value == value
+@pytest.mark.parametrize("params", [
+    (),
+    ([1],),
+    ([1, 2, 3, 4],),
+    ("abc",),
+])
+def test_init(params):
+    from queue import Queue
+    Queue(*params)
 
 
-def test_dequeue():
-    pass
+@pytest.mark.parametrize("params", [
+    (1,),
+    (1, 2),
+    ([1, 2], 3, 4),
+])
+def test_bad_init(params):
+    from queue import Queue
+    with pytest.raises(TypeError):
+        Queue(*params)
+
+
+LISTS = [
+    [],
+    [1, 1, 1],
+    list(range(10)),
+    [[1, 2], [2, 3, 4]],
+]
+
+
+@pytest.mark.parametrize("items", LISTS)
+def test_enqueue(items):
+    from queue import Queue
+    q = Queue()
+    for item in items:
+        q.enqueue(item)
+    for item in items:
+        assert q.dequeue() == item
+
+
+@pytest.mark.parametrize("items", LISTS)
+def test_dequeue(items):
+    from queue import Queue
+    q = Queue(items)
+    for item in items:
+        assert q.dequeue() == item
+    with pytest.raises(IndexError):
+        q.dequeue()
 
 
 def test_peek():
-    pass
+    from queue import Queue
+    q = Queue(range(4))
+    for _ in range(4):
+        assert q.peek() == q.dequeue()
+    # queue is now empty
+    assert q.peek() is None
 
 
 def test_size():
-    pass
+    from queue import Queue
+    q = Queue(range(4))
+    for expected in range(4, 0, -1):
+        assert q.size() == expected
+        q.dequeue()
+    # queue is now empty
+    assert q.size() == 0
