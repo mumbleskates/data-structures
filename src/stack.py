f2bd67086ff0dfb174cd126a0146700f359b67b3
Kent Ross <root.main@gmail.com>
2016-03-09 21:47:32 -0800

    wrap LinkedList rather than subclass it

--- src/stack.py
+++ src/stack.py
@@ -2,10 +2,14 @@
 from linked_list import LinkedList
 
 
-class Stack(LinkedList):
-    # Using LinkedList __init__ for Stack init method
+class Stack(object):
+    def __init__(self, items=()):
+        self._list = LinkedList(items)
 
     # push() has the same functionality as insert()
-    push = LinkedList.insert
+    def push(self, item):
+        self._list.insert(item)
 
     # pop() already has the desired functionality in LinkedList
+    def pop(self):
+        return self._list.pop()
