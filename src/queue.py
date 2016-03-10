ef33fc66ed7d8da0de978641eba12babb9af394a
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-10 10:27:45 -0800

    Stubbed out the queue.py and test_queue.py

---
+++ src/queue.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from linked_list import LinkedList
+
+# TODO: enqueue(value): adds value to the queue
+# TODO: dequeue(): removes the correct item from the queue and returns its value (should raise an error if the queue is empty)
+# TODO: peek(): returns the next value in the queue without dequeueing it. If the queue is empty, returns None
+# TODO: size(): return the size of the queue. Should return 0 if the queue is empty.
+
+
+class Queue(LinkedList):
+    def __init__(self):
+        pass
+
+    def enqueue(self, value):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
---
+++ src/test_queue.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+def test_enqueue(value):
+    assert value == value
+
+
+def test_dequeue():
+    pass
+
+
+def test_peek():
+    pass
+
+
+def test_size():
+    pass
