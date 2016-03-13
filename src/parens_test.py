9f261c0685ec22fef942c378fa3335afeafb5390
Kent Ross <root.main@gmail.com>
2016-03-12 20:22:54 -0800

    fixed test_parens for main test suite

--- src/parens_test.py
+++ src/parens_test.py
@@ -5,17 +5,17 @@ import pytest
 
 
 STRINGS = [
-    ("", 0),
-    ("()", 0),
-    ("()()()()", 0),
-    ("(())", 0),
-    ("(()", 1),
-    ("())", -1),
-    (")(", -1),
+    ("", False),
+    ("()", False),
+    ("()()()()", False),
+    ("(())", False),
+    ("(()", True),
+    ("())", True),
+    (")(", True),
 ]
 
 
 @pytest.mark.parametrize(("val", "expected"), STRINGS)
 def test_parens(val, expected):
     from parens import check_parens
-    assert check_parens(val) == expected
+    assert bool(check_parens(val)) == expected
