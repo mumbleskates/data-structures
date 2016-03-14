d8d95c0107670dfab85508c38d96da9381a81b1b
Kent Ross <root.main@gmail.com>
2016-03-14 14:45:19 -0700

    deque tests

--- src/test_deque.py
+++ src/test_deque.py
@@ -1,24 +1,92 @@
 # coding=utf-8
+from __future__ import unicode_literals
+from builtins import range
 
 import pytest
 
-def test_append(val):
-    pass
 
-def test_appendleft(val):
-    pass
+def bidirectional_match(a, b):
+    assert list(a) == list(b)
+    assert list(reversed(a)) == list(reversed(b))
+
+
+TEST_LISTS = [
+    [],
+    [1],
+    [1] * 3,
+    [1, 2, 3],
+    [1, 2, 3] * 2,
+    [1, 1, 1, 2],
+]
+
+
+@pytest.mark.parametrize("items", TEST_LISTS)
+def test_init(items):
+    from deque import Deque
+    d = Deque(items)
+    bidirectional_match(d, items)
+    assert d.size() == len(items)
+
+
+def test_append():
+    from deque import Deque
+    d = Deque()
+    expected = []
+    for item in range(5):
+        d.append(item)
+        expected.append(item)
+        bidirectional_match(d, expected)
+
+
+def test_appendleft():
+    from deque import Deque
+    d = Deque()
+    expected = []
+    for item in range(5):
+        d.appendleft(item)
+        expected.insert(item, 0)
+        bidirectional_match(d, expected)
+
 
 def test_pop():
-    pass
+    from deque import Deque
+    d = Deque(range(5))
+    expected = list(range(5))
+    while expected:
+        assert d.pop() == expected.pop()
+        assert d.size() == len(expected)
+        bidirectional_match(d, expected)
+    with pytest.raises(IndexError):
+        d.pop()
+    assert d.size() == 0
+
 
 def test_popleft():
-    pass
+    from deque import Deque
+    d = Deque(range(5))
+    expected = list(range(5))
+    while expected:
+        assert d.popleft() == expected.pop(0)
+        assert d.size() == len(expected)
+        bidirectional_match(d, expected)
+    with pytest.raises(IndexError):
+        d.popleft()
+    assert d.size() == 0
+
 
 def test_peek():
-    pass
+    from deque import Deque
+    d = Deque(range(5))
+    while d:
+        peek = d.peek()
+        assert d.pop() == peek
+    assert d.peek() is None
+
 
 def test_peekleft():
-    pass
-
-def test_size():
-    pass
+    from deque import Deque
+    d = Deque(range(5))
+    while d:
+        peek = d.peekleft()
+        assert d.popleft() == peek
+    assert d.peekleft is None
