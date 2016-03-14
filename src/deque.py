471b79c5fee717272a03fc4ba6b5bb717dd972c2
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-14 14:03:34 -0700

    test_deque file stubbed out for each funciton

---
+++ src/deque.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# TODO: append(val): adds value to the end of the deque
+# TODO: appendleft(val): adds a value to the front of the deque
+# TODO: pop(): removes a value from the end of the deque and returns it (raises an exception if the deque is empty)
+# TODO: popleft(): removes a value from the front of the deque and returns it (raises an exception if the deque is empty)
+# TODO: peek(): returns the next value that would be returned by pop but leaves the value in the deque (returns None if the deque is empty)
+# TODO: peekleft(): returns the next value that would be returned by popleft but leaves the value in the deque (returns None if the deque is empty)
+# TODO: size(): returns the count of items in the queue (returns 0 if the queue is empty)
+
+class Deque():
+    def append(val):
+        pass
+
+    def appendleft(val):
+        pass
+
+    def pop():
+        pass
+
+    def popleft():
+        pass
+
+    def peek():
+        pass
+
+    def peekleft():
+        pass
+
+    def size():
+        pass
---
+++ src/test_deque.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+import pytest
+
+def test_append(val):
+    pass
+
+def test_appendleft(val):
+    pass
+
+def test_pop():
+    pass
+
+def test_popleft():
+    pass
+
+def test_peek():
+    pass
+
+def test_peekleft():
+    pass
+
+def test_size():
+    pass
