a24c59cdd7babade9ce934ecff820354e55de458
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-16 14:21:42 -0700

    More tests.... working through the test cases

--- src/priorityq.py
+++ src/priorityq.py
@@ -1,19 +1,21 @@
 # coding=utf-8
 
 from binheap import BinHeap
+from itertools import count
 
 # DONE: .insert(item): inserts an item into the queue.
 # DONE: .pop(): removes the most important item from the queue.
 # DONE: .peek(): returns the most important item without removing it from the queue.
 
-class priotityq(object):
+class PriotityQ(object):
     def __init__(self, items=()):
         self._heap = BinHeap()
+        self._unique = count()
         for item in items:
             self.insert(item)
 
     def insert(self, item, priority=1):
-        self._heap.push((priority, item))
+        self._heap.push((priority, next(self._unique), item))
 
     def pop(self):
         priority, item = self._heap.pop()
--- src/test_priorityq.py
+++ src/test_priorityq.py
@@ -6,20 +6,43 @@ import pytest
 # TODO: .pop(): removes the most important item from the queue.
 # TODO: .peek(): returns the most important item without removing it from the queue.
 
+TEST_ITEMS = [
+    [],
+    list(zip(range(10), range(10))),
+    list(zip(reversed(range(10)), range(10))),
+    [(1, i) for i in range(5)],
+
+]
+
+
+def assert_correct(priorityq, expected):
+    # assert that the items come out as expected and in reverse sorted order
+    expected = sorted(expected)
+    while expected:
+        assert priorityq.pop() == expected.pop()
 
 
 @pytest.mark.parametrize("items", TEST_ITEMS)
-def test_init(self, items=()):
+def test_init(items):
+    from priorityq import PriotityQ
+    pq = PriotityQ(items)
+    assert_correct(pq, items)
+
+TEST_PRIORITY = [
+    ((1,"first"), "first"),
+    (((1, "first"), (2, "second"), (3, "third")), "third"),
+]
+
+@pytest.mark.parametrize("items, output", TEST_PRIORITY)
+def test_insert(items, output):
+    from priorityq import PriotityQ
+    pq = PriotityQ(items)
+    assert pq.pop() == output
+
+
+def test_pop():
     pass
 
 
-def test_insert(self, item):
-    pass
-
-
-def test_pop(self):
-    pass
-
-
-def test_peek(self):
+def test_peek():
     pass
