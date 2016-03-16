965a6e0fc6cf5d06fbcbe80c47553920ea35346d
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-15 21:42:26 -0700

    Added paren.py Added test_paren.py Added tests for paren to test_paren.py

---
+++ src/paren.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+# TODO: Return 1 if the string is “open” (there are open parens that are not closed)
+# TODO: Return 0 if the string is “balanced” (there are an equal number of open and closed parentheses in the string)
+# TODO: Return -1 if the string is “broken” (a closing parens has not been proceeded by one that opens)
---
+++ src/test_paren.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+import pytest
+
+PROPER_PAREN_OPEN = [
+    ("("),
+    ("(()"),
+    ("(((())"),
+]
+
+
+@pytest.mark.parametrize("arg", PROPER_PAREN_OPEN)
+def test_proper_paren(arg):
+    assert proper_paren(arg) == 1
+
+
+PROPER_PAREN_BALLANCED = [
+    ("()"),
+    ("(())"),
+    ("()(())"),
+    ("()()"),
+]
+
+
+@pytest.mark.parametrize("arg", PROPER_PAREN_BALLANCED)
+def test_proper_paren(arg):
+    assert proper_paren(arg) == 0
+
+
+PROPER_PAREN_CLOSED = [
+    (")"),
+    ("())"),
+    ("()())"),
+    ("(()))"),
+]
+
+
+@pytest.mark.parametrize("arg", PROPER_PAREN_CLOSED)
+def test_proper_paren(arg):
+    assert proper_paren(arg) == -1
