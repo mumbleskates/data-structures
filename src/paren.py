775a911a5949424f56fdd3cb0601175fceca7689
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-15 21:59:49 -0700

    Wrote first pass of proper_paren in paren.py Import proper_paren on each test in test_paren.py

--- src/paren.py
+++ src/paren.py
@@ -1,5 +1,21 @@
 # coding=utf-8
 
-# TODO: Return 1 if the string is “open” (there are open parens that are not closed)
-# TODO: Return 0 if the string is “balanced” (there are an equal number of open and closed parentheses in the string)
-# TODO: Return -1 if the string is “broken” (a closing parens has not been proceeded by one that opens)
+# DONE: Return 1 if the string is “open” (there are open parens that are not closed)
+# DONE: Return 0 if the string is “balanced” (there are an equal number of open and closed parentheses in the string)
+# DONE: Return -1 if the string is “broken” (a closing parens has not been proceeded by one that opens)
+
+
+def proper_paren(test_string):
+    left_count = 0
+    right_count = 0
+    for char in test_string:
+        if char is "(":
+            left_count += 1
+        if char is ")":
+            right_count += 1
+        if left_count < right_count:
+            return 1
+    if left_count > right_count:
+        return -1
+    else:
+        return 0
--- src/test_paren.py
+++ src/test_paren.py
@@ -11,6 +11,7 @@ PROPER_PAREN_OPEN = [
 
 @pytest.mark.parametrize("arg", PROPER_PAREN_OPEN)
 def test_proper_paren(arg):
+    from paren import proper_paren
     assert proper_paren(arg) == 1
 
 
@@ -24,6 +25,7 @@ PROPER_PAREN_BALLANCED = [
 
 @pytest.mark.parametrize("arg", PROPER_PAREN_BALLANCED)
 def test_proper_paren(arg):
+    from paren import proper_paren
     assert proper_paren(arg) == 0
 
 
@@ -37,4 +39,5 @@ PROPER_PAREN_CLOSED = [
 
 @pytest.mark.parametrize("arg", PROPER_PAREN_CLOSED)
 def test_proper_paren(arg):
+    from paren import proper_paren
     assert proper_paren(arg) == -1
