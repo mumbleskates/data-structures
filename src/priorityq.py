74ad10ebc53d7573259bb8876afd00cead3623b5
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-16 13:45:52 -0700

    Stubbed out the test_priorityq and priorityq

---
+++ src/priorityq.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+# TODO: .insert(item): inserts an item into the queue.
+# TODO: .pop(): removes the most important item from the queue.
+# TODO: .peek(): returns the most important item without removing it from the queue.
+
+class priotityq(object):
+    def __init__(self, items=()):
+        pass
+
+    def insert(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
---
+++ src/test_priorityq.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import pytest
+
+# TODO: .insert(item): inserts an item into the queue.
+# TODO: .pop(): removes the most important item from the queue.
+# TODO: .peek(): returns the most important item without removing it from the queue.
+
+
+def test_init(self, items=()):
+    pass
+
+
+def test_insert(self, item):
+    pass
+
+
+def test_pop(self):
+    pass
+
+
+def test_peek(self):
+    pass
