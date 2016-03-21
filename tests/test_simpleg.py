6c6fab0e1bf79a07f373892e6fad1e0d83bb57b1
Kent Ross <root.main@gmail.com>
2016-03-21 15:24:47 -0700

    move perf test into test files

--- tests/test_simpleg.py
+++ tests/test_simpleg.py
@@ -140,3 +140,57 @@ def test_breadth_first_traversal(demo_graph):
     assert result[5] in [3, 4, 5, 6]
     assert result[6] in [3, 4, 5, 6]
     assert len(result) == 7
+
+
+def _main():
+    from functools import partial
+    import json
+    from timeit import timeit
+    from data_structures.simpleg import Graph
+
+    def print_performance():
+        depth = timeit(partial(g.depth_first_traversal, (start,)), number=1000000)
+        breadth = timeit(partial(g.breadth_first_traversal, (start,)), number=1000000)
+        print("Depth first:   {}\n"
+              "Breadth first: {}\n"
+              "depth/breadth: {}".format(depth, breadth, depth/breadth))
+
+    print("Performance tests!")
+    start = 0
+    for branch_factor in [1, 2, 10]:
+        print("12,000 nodes in a tree with branching factor {}".format(branch_factor))
+        # build tree
+        g = Graph()
+        for parent in range(int(12000 / branch_factor + 1)):
+            for child in (parent * branch_factor + 1 + x for x in range(branch_factor)):
+                if child < 12000:
+                    g.add_edge(parent, child)
+
+        print_performance()
+        print()
+
+    print("~12000 nodes in a map-like graph:")
+    # build graph
+    g = Graph()
+    with open("test_data/graph_data.json", 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    start = data['start']
+    data = data['data']
+    for edge in data:
+        g.add_edge(edge['from'], edge['to'])
+
+    print_performance()
+    print()
+
+    print("1000 nodes all connected to each other:")
+    g = Graph()
+    for i in range(1000):
+        for j in range(1000):
+            if i != j:
+                g.add_edge(i, j)
+
+    print_performance()
+
+
+if __name__ == '__main__':
+    _main()
