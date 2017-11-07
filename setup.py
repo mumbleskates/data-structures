28b0b30e3f81b490e35d69e3ba1a15d1e3069418
Kent Ross <k@mad.cash>
2017-11-06 17:42:57 -0800

    consolidate requirements in setup.py, freeze & update versions

--- setup.py
+++ setup.py
@@ -12,6 +12,12 @@ setup(
     package_dir={"": "src"},
     install_requires=['future'],
     extras_require={
-        'test': ['pytest-cov', 'tox', 'mock']
+        'test': [
+            'coverage==4.4.2',
+            'pytest==3.2.3',
+            'pytest-cov==2.5.1',
+            'tox==2.9.1',
+            'mock==2.0.0',
+        ]
     },
 )
